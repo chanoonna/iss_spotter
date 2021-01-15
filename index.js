96cdfb0bd6d811a8c36476854ddb6c51447832cc
chanoonna <chanoonna@gmail.com>
2021-01-15 02:50:07 +0000

    added requests for ISS location and next pass-by time

--- index.js
+++ index.js
@@ -1,20 +1,25 @@
 const {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  initializeISS,
 } = require('./iss');
 
-fetchMyIP((error, ip) => {
+const getIP = function(error, ip) {                         // Callback for fetchMyIP
+  if (error) {
+    console.log("It didn't work!", error);                  // Being lazy with error handling.
+    return;
+  }
+
+  fetchCoordsByIP(ip, getCoords);                           // Fetch coordinates with ip. (callback: getCoords)
+};
+
+const getCoords = function(error, coordinates) {            // Callback for getting coordinates
   if (error) {
     console.log("It didn't work!", error);
     return;
   }
 
-  fetchCoordsByIP(ip, (error, coordinates) => {
-    if (error) {
-      console.log("It didn't work!", error);
-      return;
-    }
+  initializeISS(coordinates);                               // Call initializeISS to request ISS data with the coordinates.
+};
 
-    console.log(coordinates);
-  });
-});
+fetchMyIP(getIP);                                           // Starting Point.
--- iss.js
+++ iss.js
@@ -35,7 +35,54 @@ const fetchCoordsByIP = function(ip, callback) {
   });
 };
 
+const initializeISS = function(coords) {
+  issPassBy(coords);                                                                        // This will printout ISS Pass-by Time (Prints once);
+  setInterval(                                                                              // Set interval here to fetch ISS Location in every sec.
+    () => request('http://api.open-notify.org/astros.json', (error, response, body) => {    // This is for the number of people in space.
+      if (error || response.statusCode !== 200) {
+        console.log(error);                                                                 // If there's an error, exit the app.
+        process.exit();
+      }
+      const spacePeople = JSON.parse(body).number;
+      issLocation(spacePeople);                                                             // With then number with got from the server, call issLocation.
+      return;
+    })
+    , 1000);                                                                                  // 1000 milsec for setInterval.
+};
+
+const issLocation = function(people) {
+  request('http://api.open-notify.org/iss-now.json', (error, response, body) => {           // Request ISS Location.
+    if (error || response.statusCode !== 200) {
+      process.stdout.write(`\rCan't fetch data.`);                                          // If an error, it will show error message. But don't quit.
+      return;
+    }
+    const parsed = JSON.parse(body).iss_position;
+    const issLoc = { latitude: parsed.latitude, longitude: parsed.longitude };
+    process.stdout.write(`\rSpace people: ${people} || ISS Location: Latitude ${issLoc.latitude}, Longitude ${issLoc.longitude}`);
+  });                                                                                       // Print the current location of ISS. (On the same line);
+};
+
+const issPassBy = function(coords) {                                                        // This is for fetching Next ISS Pass-bys
+  request(`http://api.open-notify.org/iss-pass.json?lat=${coords.latitude}&lon=${coords.longitude}`, (error, response, body) => {
+    if (error || response.statusCode !== 200) {
+      console.log(`Failed to fetch ISS Passtime at ${coords.latitude}, ${coords.longitude}`);   // Another example being lazy.
+      return;
+    }
+    printPassBy([...JSON.parse(body).response]);                                            // Call print function. Data is a form of array.
+  });
+};
+
+const printPassBy = function(arr) {
+  console.log(`ISS PASS TIME (VANCOUVER/PASIFIC)`);
+  arr.forEach((timestamp) => {
+    let riseTime = new Date(timestamp.risetime * 1000).toLocaleString('en-US', { timeZone: 'America/Vancouver' });
+    let duration = timestamp.duration;
+    console.log(`Duration: ${duration} sec || ${riseTime}`);
+  });
+};
+
 module.exports = {
   fetchMyIP,
-  fetchCoordsByIP
+  fetchCoordsByIP,
+  initializeISS
 };
